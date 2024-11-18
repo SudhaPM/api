939cf341615a883421dd36a84ea51d3a45116456
SudhaPM <108056697+SudhaPM@users.noreply.github.com>
2024-11-18 05:50:00 -0800

    Update GlobalExceptionHandler.java

--- coupon-management-api/src/main/java/com/example/coupon_management_api/exception/GlobalExceptionHandler.java
+++ coupon-management-api/src/main/java/com/example/coupon_management_api/exception/GlobalExceptionHandler.java
@@ -1,22 +1,33 @@
 package com.example.coupon_management_api.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    //Resourcenot found Exception
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<Map<String, Object>> handleResourceNotFoundException(ResourceNotFoundException ex){
-        return buildErrorResponse(ex.getMessage());
+        Map<String,Object> response = new HashMap<>();
+        response.put("error", "Resource Not Found");
+        response.put("message",ex.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
 
-    private ResponseEntity<Map<String, Object>> buildErrorResponse(String message) {
-        return null;
+    //Invalid argument is passed
+    public ResponseEntity<Map<String,Object>> handleIllegalException(IllegalArgumentException ex){
+
+        Map<String,Object> response = new HashMap<>();
+        response.put("error", "Illegal Argument");
+        response.put("message",ex.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 
 }
